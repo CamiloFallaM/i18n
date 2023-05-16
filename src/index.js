617582af9dd01c12a0ca60c65399bd6763084922
CamiloFallaM <c.falla@uniandes.edu.co>
2023-05-16 09:20:51 -0500

    CamiloFalla

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const userLanguage = navigator.language;
+
+if(userLanguage == "es-ES")
+{
+    ReactDOM.render(
+        <IntlProvider locale="es-ES" messages= {localeEsMessages}>
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+    );
+}
+else if(userLanguage == "en-US")
+{
+    ReactDOM.render(
+        <IntlProvider locale="en-US" messages= {localeEnMessages}>
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+    );
+}
+
+
+
+
